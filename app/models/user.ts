805c2109656accd748813bfb60e9f8b16df5576d
Justin Menard <justinmenard@orange.fr>
2024-03-06 19:35:03 +0100

    Setup database and user model

--- app/models/user.ts
+++ app/models/user.ts
@@ -15,7 +15,13 @@ export default class User extends compose(BaseModel, AuthFinder) {
   declare id: number
 
   @column()
-  declare fullName: string | null
+  declare username: string | null
+
+  @column()
+  declare firstname: string | null
+
+  @column()
+  declare lastname: string | null
 
   @column()
   declare email: string
